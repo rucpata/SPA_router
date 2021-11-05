10d69bdf8fa05eb1ee13edd56ec6d36428336b66
rucpata <rucinska.patrycja@gmail.com>
2021-11-05 12:09:23 +0100

    created structur

---
+++ src/layouts/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import '../styles/App.css';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <header>
+            {<Header/>}
+          </header>
+          <main>
+            <aside>
+              {<Navigation/>}
+            </aside>
+            <section className='page'>
+              {<Page/>}
+            </section>
+          </main>
+          <footer>
+            {<Footer/>}
+          </footer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
