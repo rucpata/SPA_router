83baf250caf632dad23d74dcd3a99e0d6ca70ceb
rucpata <rucinska.patrycja@gmail.com>
2021-11-05 15:17:47 +0100

    ContactPage and Prompt

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,15 +1,39 @@
 import React from 'react';
-
-
+import '../styles/ContactPage.css';
+import {Prompt} from 'react-router-dom';
 
 class ContactPage extends React.Component {
+    state = {
+        value: '',
+    }
     
+    handleChange = e => {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState({
+            value: ''
+        })
+    }
+
     render() {
         return (
             <div className="contact">
-                contact page
+                <h3>Napisz do nas</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <textarea value={this.state.value} onChange={this.handleChange}>Wpisz wiadomość</textarea>
+                    <button >Wyślij</button>
+                </form>
+                <Prompt 
+                    when={this.state.value}
+                    message="Masz niewypełniony formuarz. Czy chcesz opuścić tę stronę?"
+                />
             </div>
-
+            
         );
     }
 }
