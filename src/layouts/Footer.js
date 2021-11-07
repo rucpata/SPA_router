13193783e8bb842438dd8a1a6bb122bf21664381
rucpata <rucinska.patrycja@gmail.com>
2021-11-07 14:37:08 +0100

    footer - router

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
 
 const Footer = () => {
     return (
         <div>
-            stopka
+            <h2>Stopka</h2>
+            <Route path='/' exact render={(props)=> <p>Jesteś na stronie głównej</p>
+                }/>
+            <Route path='/:page' exact render={(props)=> {
+                console.log(props);
+                return(
+                    <p>Jesteś na stronie <span>{props.match.params.page}</span></p>
+                )
+            }}/>
+
+            <Route path='/:page/:idProduct' exact render={(props)=> {
+                console.log(props);
+                return(
+                    <p>Jesteś na stronie <span>{props.match.params.idProduct}</span></p>
+                )
+            }}/>
         </div>
     )
 }
